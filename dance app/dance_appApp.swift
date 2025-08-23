f5a701da705d8b135c2e9dd42c37523d7aa9a0b6
adhavan950 <adhavan_senthil_kumar@students.edu.sg>
2025-08-23 09:25:49 +0800

    Initial Commit

---
+++ dance app/dance_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  dance_appApp.swift
+//  dance app
+//
+//  Created by Adhavan senthil kumar on 23/8/25.
+//
+
+import SwiftUI
+
+@main
+struct dance_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
