f479b1078e142e22055c01a53336dcc75aa9f1bb
adhavan950 <adhavan_senthil_kumar@students.edu.sg>
2025-09-08 09:32:56 +0800

    final app

---
+++ lwk dancing/Views/loadingView.swift
@@ -0,0 +1,18 @@
+//
+//  loadingView.swift
+//  lwk dancing
+//
+//  Created by Adhavan senthil kumar on 8/9/25.
+//
+
+import SwiftUI
+
+struct loadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loadingView()
+}
